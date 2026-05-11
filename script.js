b866fb6079988d0dc5dea8c9cc187106ac89e0ec
ctrae <traefletch2006@gmail.com>
2026-05-10 22:27:50 -0500

    new update

--- script.js
+++ script.js
@@ -1,7 +1,53 @@
-// Mobile Menu Toggle
+// MOBILE MENU
 const menuToggle = document.getElementById("menu-toggle");
 const navLinks = document.getElementById("nav-links");
 
 menuToggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
+});
+
+
+// MUSIC SYSTEM
+const music = document.getElementById("bg-music");
+const musicBtn = document.getElementById("music-btn");
+
+
+// ATTEMPT AUTOPLAY
+window.addEventListener("load", () => {
+
+    music.volume = 0.5;
+
+    const playPromise = music.play();
+
+    if (playPromise !== undefined) {
+
+        playPromise.catch(() => {
+
+            musicBtn.textContent =
+            "Play Music 🎵";
+
+        });
+
+    }
+
+});
+
+
+// BUTTON TOGGLE
+musicBtn.addEventListener("click", () => {
+
+    if (music.paused) {
+
+        music.play();
+
+        musicBtn.textContent =
+        "Pause ⏸";
+
+    } else {
+
+        music.pause();
+
+        musicBtn.textContent =
+        "Play 🎵";
+    }
 });
